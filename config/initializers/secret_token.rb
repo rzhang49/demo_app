20228ff403bcc6f9a77668e13e5d624063b9dcb0
onecare <rzhang49@gmail.com>
2013-02-19 17:24:02 -0500

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '0d98c557b24a2d40d085d199ce92b73b36dddc1e312085810d88577d5e473440293412400ddd307907da8d2091391e2560e7705380abc760f341fe72b83bb32a'
